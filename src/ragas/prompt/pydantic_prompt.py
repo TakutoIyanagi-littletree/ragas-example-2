0342d8979adfa1223a7155a06c8023e6ad4d3d1c
TakutoIyanagi-littletree <131866009+TakutoIyanagi-littletree@users.noreply.github.com>
2024-10-25 02:53:33 +0900

    Update pydantic_prompt.py

--- src/ragas/prompt/pydantic_prompt.py
+++ src/ragas/prompt/pydantic_prompt.py
@@ -76,13 +76,13 @@ class PydanticPrompt(BasePrompt, t.Generic[InputModel, OutputModel]):
             + self._generate_output_signature()
             + "\n"
             + self._generate_examples()
-            + "\nNow perform the above instruction with the following input\n"
+            + "\n\nNow perform the above instruction with the following input\n"
             + (
                 "input: " + data.model_dump_json(indent=4) + "\n"
                 if data is not None
                 else "input: (None)\n"
             )
-            + "output: "
+            + "\noutput: "
         )
 
     async def generate(
